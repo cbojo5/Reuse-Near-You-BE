8cd0a691fed54c7ea7dd9c59fae9e91ea8fd4263
Georgia Adam <georgiaeadam@gmail.com>
2021-10-19 01:19:08 -0400

    updated database

--- locator/models.py
+++ locator/models.py
@@ -4,5 +4,6 @@ from django.db import models
 class Location(models.Model):
     name = models.CharField(max_length=250)
     address = models.TextField()
+    items_accepted = models.TextField(default='')
     latitude = models.FloatField()
     longitude = models.FloatField()
